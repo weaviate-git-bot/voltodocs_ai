a275bf606b9f6cba5b3adfd082d196f10751da00
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-08 12:04:03 +0200

    Builds

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
+pip install virtualenv
+virtualenv .venv
+
 .venv/bin/pip install -U pip
 .venv/bin/pip install sentence_transformers
-.venv/bin/pip install  torch  --index-url https://download.pytorch.org/whl/cu118
+.venv/bin/pip install torch --index-url https://download.pytorch.org/whl/cu118
 .venv/bin/pip install InstructorEmbedding
 .venv/bin/pip install langserve
 .venv/bin/pip install weaviate-client
 .venv/bin/pip install -e packages/voltodocs/
-virtualenv .venv
-pip install virtualenv
-.venv/bin/python app/server.py
 .venv/bin/pip install -U langchain-community
 .venv/bin/pip install -U faiss-gpu
+.venv/bin/pip install llama-cpp-python --prefer-binary --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/AVX2/cu122
+
+.venv/bin/python app/server.py
+.venv/bin/pip install sse_starlette
