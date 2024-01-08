bf5f102474c4e27ad9ad1ba8454da5ac12292efa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-08 17:56:09 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ virtualenv .venv
 .venv/bin/pip install -U faiss-gpu
 .venv/bin/pip install llama-cpp-python --prefer-binary --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/AVX2/cu122
 
-.venv/bin/python app/server.py
 .venv/bin/pip install sse_starlette
 .venv/bin/pip install git+https://github.com/huggingface/transformers
 .venv/bin/pip install ctransformers
+
+#.venv/bin/python app/server.py
